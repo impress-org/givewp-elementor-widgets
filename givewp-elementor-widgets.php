74fae6b7303ebaec7a0aea19e768e391d7439f43
Matt Cromwell <reachme@mattcromwell.com>
2020-04-23 22:48:21 -0700

    Clean up main file formatting.

--- givewp-elementor-widgets.php
+++ givewp-elementor-widgets.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: 	Donation Widgets for Elementor and GiveWP 
  * Plugin URI: 		https://givewp.com
@@ -16,14 +17,15 @@
  */
 
 // Exit if access directly.
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 /**
  * Class GiveWP_DW_4_Elementor
  */
-final class GiveWP_DW_4_Elementor {
+final class GiveWP_DW_4_Elementor
+{
 	/**
 	 * Instance.
 	 *
@@ -39,10 +41,11 @@ final class GiveWP_DW_4_Elementor {
 	 * @since
 	 * @access private
 	 */
-	private function __construct() {
-    }
-    
-    /**
+	private function __construct()
+	{
+	}
+
+	/**
 	 * Minimum Elementor Version
 	 *
 	 * @since 1.0.0
@@ -60,8 +63,9 @@ final class GiveWP_DW_4_Elementor {
 	 * @access public
 	 *
 	 */
-	public static function get_instance() {
-		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof GiveWP_DW_4_Elementor ) ) {
+	public static function get_instance()
+	{
+		if (!isset(self::$instance) && !(self::$instance instanceof GiveWP_DW_4_Elementor)) {
 			self::$instance = new GiveWP_DW_4_Elementor();
 			self::$instance->setup();
 		}
@@ -76,11 +80,12 @@ final class GiveWP_DW_4_Elementor {
 	 * @since
 	 * @access private
 	 */
-	private function setup() {
+	private function setup()
+	{
 		self::$instance->setup_constants();
 
-		register_activation_hook( GiveWP_DW_4_Elementor_FILE, array( $this, 'install' ) );
-		add_action( 'give_init', array( $this, 'init' ), 10, 1 );
+		register_activation_hook(GiveWP_DW_4_Elementor_FILE, array($this, 'install'));
+		add_action('give_init', array($this, 'init'), 10, 1);
 	}
 
 
@@ -92,36 +97,37 @@ final class GiveWP_DW_4_Elementor {
 	 * @since
 	 * @access private
 	 */
-	private function setup_constants() {
+	private function setup_constants()
+	{
 
 		// Defines addon version number for easy reference.
-		if ( ! defined( 'GiveWP_DW_4_Elementor_VERSION' ) ) {
-			define( 'GiveWP_DW_4_Elementor_VERSION', '1.0' );
+		if (!defined('GiveWP_DW_4_Elementor_VERSION')) {
+			define('GiveWP_DW_4_Elementor_VERSION', '1.0');
 		}
 
 		// Set it to latest.
-		if ( ! defined( 'GiveWP_DW_4_Elementor_MIN_GIVE_VERSION' ) ) {
-			define( 'GiveWP_DW_4_Elementor_MIN_GIVE_VERSION', '2.5' );
+		if (!defined('GiveWP_DW_4_Elementor_MIN_GIVE_VERSION')) {
+			define('GiveWP_DW_4_Elementor_MIN_GIVE_VERSION', '2.5');
 		}
 
-		if ( ! defined( 'GiveWP_DW_4_Elementor_FILE' ) ) {
-			define( 'GiveWP_DW_4_Elementor_FILE', __FILE__ );
+		if (!defined('GiveWP_DW_4_Elementor_FILE')) {
+			define('GiveWP_DW_4_Elementor_FILE', __FILE__);
 		}
 
-		if ( ! defined( 'GiveWP_DW_4_Elementor_DIR' ) ) {
-			define( 'GiveWP_DW_4_Elementor_DIR', plugin_dir_path( GiveWP_DW_4_Elementor_FILE ) );
+		if (!defined('GiveWP_DW_4_Elementor_DIR')) {
+			define('GiveWP_DW_4_Elementor_DIR', plugin_dir_path(GiveWP_DW_4_Elementor_FILE));
 		}
 
-		if ( ! defined( 'GiveWP_DW_4_Elementor_Widgets_Folder' ) ) {
-			define( 'GiveWP_DW_4_Elementor_Widgets_Folder', plugin_dir_path( GiveWP_DW_4_Elementor_FILE ) . '/widgets/' );
+		if (!defined('GiveWP_DW_4_Elementor_Widgets_Folder')) {
+			define('GiveWP_DW_4_Elementor_Widgets_Folder', plugin_dir_path(GiveWP_DW_4_Elementor_FILE) . '/widgets/');
 		}
 
-		if ( ! defined( 'GiveWP_DW_4_Elementor_URL' ) ) {
-			define( 'GiveWP_DW_4_Elementor_URL', plugin_dir_url( GiveWP_DW_4_Elementor_FILE ) );
+		if (!defined('GiveWP_DW_4_Elementor_URL')) {
+			define('GiveWP_DW_4_Elementor_URL', plugin_dir_url(GiveWP_DW_4_Elementor_FILE));
 		}
 
-		if ( ! defined( 'GiveWP_DW_4_Elementor_BASENAME' ) ) {
-			define( 'GiveWP_DW_4_Elementor_BASENAME', plugin_basename( GiveWP_DW_4_Elementor_FILE ) );
+		if (!defined('GiveWP_DW_4_Elementor_BASENAME')) {
+			define('GiveWP_DW_4_Elementor_BASENAME', plugin_basename(GiveWP_DW_4_Elementor_FILE));
 		}
 	}
 
@@ -131,12 +137,12 @@ final class GiveWP_DW_4_Elementor {
 	 * @since
 	 * @access public
 	 */
-	public function install() {
+	public function install()
+	{
 		// Bailout.
-		if ( ! self::$instance->check_environment() ) {
+		if (!self::$instance->check_environment()) {
 			return;
 		}
-		
 	}
 
 	/**
@@ -149,33 +155,33 @@ final class GiveWP_DW_4_Elementor {
 	 * @access public
 	 *
 	 */
-	public function init( $give ) {
+	public function init($give)
+	{
 
 		self::$instance->load_files();
 
 		// Set up localization.
-        $this->load_textdomain();
-        
-        // Check if Elementor installed and activated
-		if ( ! did_action( 'elementor/loaded' ) ) {
-			add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
+		$this->load_textdomain();
+
+		// Check if Elementor installed and activated
+		if (!did_action('elementor/loaded')) {
+			add_action('admin_notices', [$this, 'admin_notice_missing_main_plugin']);
 			return;
 		}
 
 		// Check for required Elementor version
-		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
-			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
+		if (!version_compare(ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=')) {
+			add_action('admin_notices', [$this, 'admin_notice_minimum_elementor_version']);
 			return;
-        }
-        
-        // Add Plugin actions
-		add_action( 'elementor/widgets/widgets_registered', [ $this, 'init_widgets' ] );
+		}
+
+		// Add Plugin actions
+		add_action('elementor/widgets/widgets_registered', [$this, 'init_widgets']);
 
 		add_action('elementor/editor/before_enqueue_scripts', array($this, 'editor_enqueue_scripts'));
-	
-    }
-    
-    /**
+	}
+
+	/**
 	 * Admin notice
 	 *
 	 * Warning when the site doesn't have Elementor installed or activated.
@@ -184,19 +190,19 @@ final class GiveWP_DW_4_Elementor {
 	 *
 	 * @access public
 	 */
-	public function admin_notice_missing_main_plugin() {
+	public function admin_notice_missing_main_plugin()
+	{
 
-		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+		if (isset($_GET['activate'])) unset($_GET['activate']);
 
 		$message = sprintf(
 			/* translators: 1: Plugin name 2: Elementor */
-			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementor-test-extension' ),
-			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementor-test-extension' ) . '</strong>',
-			'<strong>' . esc_html__( 'Elementor', 'elementor-test-extension' ) . '</strong>'
+			esc_html__('"%1$s" requires "%2$s" to be installed and activated.', 'elementor-test-extension'),
+			'<strong>' . esc_html__('Elementor Test Extension', 'elementor-test-extension') . '</strong>',
+			'<strong>' . esc_html__('Elementor', 'elementor-test-extension') . '</strong>'
 		);
 
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
-
+		printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
 	}
 
 	/**
@@ -208,46 +214,46 @@ final class GiveWP_DW_4_Elementor {
 	 *
 	 * @access public
 	 */
-	public function admin_notice_minimum_elementor_version() {
+	public function admin_notice_minimum_elementor_version()
+	{
 
-		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+		if (isset($_GET['activate'])) unset($_GET['activate']);
 
 		$message = sprintf(
 			/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
-			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-test-extension' ),
-			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementor-test-extension' ) . '</strong>',
-			'<strong>' . esc_html__( 'Elementor', 'elementor-test-extension' ) . '</strong>',
-			 self::MINIMUM_ELEMENTOR_VERSION
+			esc_html__('"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-test-extension'),
+			'<strong>' . esc_html__('Elementor Test Extension', 'elementor-test-extension') . '</strong>',
+			'<strong>' . esc_html__('Elementor', 'elementor-test-extension') . '</strong>',
+			self::MINIMUM_ELEMENTOR_VERSION
 		);
 
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
-
+		printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
 	}
 
 
 	/**
-     * Loads the plugin language files.
-     *
-     * @since  1.0
-     * @access public
-     *
-     * @return void
-     */
-    public function load_textdomain() {
+	 * Loads the plugin language files.
+	 *
+	 * @since  1.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function load_textdomain()
+	{
 
-        // Set filter for Give's languages directory
-        $give_lang_dir = dirname( plugin_basename( GiveWP_DW_4_Elementor_FILE ) ) . '/languages/';
-        $give_lang_dir = apply_filters( 'dw4elementor_languages_directory', $give_lang_dir );
+		// Set filter for Give's languages directory
+		$give_lang_dir = dirname(plugin_basename(GiveWP_DW_4_Elementor_FILE)) . '/languages/';
+		$give_lang_dir = apply_filters('dw4elementor_languages_directory', $give_lang_dir);
 
-        // Traditional WordPress plugin locale filter.
-        $locale = is_admin() && function_exists( 'get_user_locale' ) ? get_user_locale() : get_locale();
-        $locale = apply_filters( 'plugin_locale', $locale, 'dw4elementor' );
+		// Traditional WordPress plugin locale filter.
+		$locale = is_admin() && function_exists('get_user_locale') ? get_user_locale() : get_locale();
+		$locale = apply_filters('plugin_locale', $locale, 'dw4elementor');
 
-        unload_textdomain( 'dw4elementor' );
-        load_textdomain( 'dw4elementor', WP_LANG_DIR . '/donation-widgets-for-elementor/' . $locale . '.mo' );
-        load_plugin_textdomain( 'dw4elementor', false, $give_lang_dir );
-
-    }
+		unload_textdomain('dw4elementor');
+		load_textdomain('dw4elementor', WP_LANG_DIR . '/donation-widgets-for-elementor/' . $locale . '.mo');
+		load_plugin_textdomain('dw4elementor', false, $give_lang_dir);
+	}
 
 
 	/**
@@ -258,11 +264,12 @@ final class GiveWP_DW_4_Elementor {
 	 *
 	 * @return bool
 	 */
-	public function check_environment() {
+	public function check_environment()
+	{
 		// Flag to check whether plugin file is loaded or not.
 		$is_working = true;
 		// Load plugin helper functions.
-		if ( ! function_exists( 'is_plugin_active' ) ) {
+		if (!function_exists('is_plugin_active')) {
 			require_once ABSPATH . '/wp-admin/includes/plugin.php';
 		}
 
@@ -275,11 +282,12 @@ final class GiveWP_DW_4_Elementor {
 	 * @since
 	 * @access private
 	 */
-	private function load_files() {
+	private function load_files()
+	{
 		require_once GiveWP_DW_4_Elementor_DIR . 'includes/helper-functions.php';
 	}
-    
-    /**
+
+	/**
 	 * Init Widgets
 	 *
 	 * Include widgets files and register them
@@ -288,46 +296,46 @@ final class GiveWP_DW_4_Elementor {
 	 *
 	 * @access public
 	 */
-	public function init_widgets() {
+	public function init_widgets()
+	{
 
 		// Include Widget files
-		$dir = GiveWP_DW_4_Elementor_Widgets_Folder; 
+		$dir = GiveWP_DW_4_Elementor_Widgets_Folder;
 
 		$files = glob($dir . '/*.php');
 
 		foreach ($files as $file) {
-			require($file);   
+			require($file);
 		}
 
 		// Register Donation History widget
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \DW4Elementor_Donation_History_Widget() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \DW4Elementor_Donation_History_Widget());
 
 		// Register Receipt widget
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \DW4Elementor_GiveWP_Receipt_Widget() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \DW4Elementor_GiveWP_Receipt_Widget());
 
 		// Register Totals widget
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \DW4Elementor_GiveWP_Totals_Widget() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \DW4Elementor_GiveWP_Totals_Widget());
 
 		// Register Register widget
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \DW4Elementor_GiveWP_Register_Widget() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \DW4Elementor_GiveWP_Register_Widget());
 
 		// Register Login widget
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \DW4Elementor_GiveWP_Login_Widget() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \DW4Elementor_GiveWP_Login_Widget());
 
 		// Register Profile Editor widget
-		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \DW4Elementor_GiveWP_Profile_Editor_Widget() );
-
-    }
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \DW4Elementor_GiveWP_Profile_Editor_Widget());
+	}
 
 	// editor styles
-    public function editor_enqueue_scripts() {
+	public function editor_enqueue_scripts()
+	{
 
-		wp_enqueue_style( 'give-admin-styles', GIVE_PLUGIN_URL . 'assets/dist/css/admin.rtl.css', array(), GIVE_VERSION );
+		wp_enqueue_style('give-admin-styles', GIVE_PLUGIN_URL . 'assets/dist/css/admin.rtl.css', array(), GIVE_VERSION);
 
 		// admin editor styles
-        wp_enqueue_style('dw4elementor-admin-styles', GiveWP_DW_4_Elementor_URL . '/assets/dw4elementor-admin.css', array('give-admin-styles') , mt_rand(9,999) );
-    }
-
+		wp_enqueue_style('dw4elementor-admin-styles', GiveWP_DW_4_Elementor_URL . '/assets/dw4elementor-admin.css', array('give-admin-styles'), mt_rand(9, 999));
+	}
 }
 
 /**
@@ -343,7 +351,8 @@ final class GiveWP_DW_4_Elementor {
  * @since 1.0
  *
  */
-function GiveWP_DW_4_Elementor() {
+function GiveWP_DW_4_Elementor()
+{
 	return GiveWP_DW_4_Elementor::get_instance();
 }
 
